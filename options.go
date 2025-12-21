bc0495ff37a4709257e9bab1946868733d0c4f25
Mikołaj Gąsior <m@gasior.dev>
2025-12-21 02:32:47 +0100

    Tweak comment

--- options.go
+++ options.go
@@ -1,6 +1,6 @@
 package validator
 
-// Optional configuration for validation:
+// ValidateOptions is an optional configuration for validation:
 // * RestrictFields defines what struct fields should be validated
 // * TagName sets tag used to define validation (default is "validation")
 type ValidateOptions struct {
